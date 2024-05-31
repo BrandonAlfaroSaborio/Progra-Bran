65d19f2e7c8c84195a357a2821ded3fb557cf08f
Bran <brandonalfarosaborio@gmail.com>
2024-05-31 16:55:41 -0600

    cambio

--- Repaso objetos.py
+++ Repaso objetos.py
@@ -7,19 +7,23 @@ class Pajaro:
         self.forma_del_pico= forma_del_pico
         self.cantidad_plumas= cantidad_plumas 
 
-    def crecimineto(self, nueava_envergadura, nueva_cantidad_plumas, nuevo_color):
+    def crecimiento(self, nueava_envergadura, nueva_cantidad_plumas, nuevo_color):
         self.nueva_envergadura = nueava_envergadura
         self.nueva_cantidad_plumas= nueva_cantidad_plumas
         self.nuevo_color= nuevo_color
 
-    def descripcion (self):
-        print("Color")
+#Convierte a strings 
+    def descripcion(self):
+	    print("Color: %s\nForma Pico: %s\nEspecie: %s\nSubespecie: %s\nEnvergadura: %f\nCantidad Plumas: %i" % (self.color, self.forma_del_pico, self.especie, self.subespecie, self.envergadura, self.cantidad_plumas))
 
 #Self NUNCA se envia como parametro
 pajarito1=Pajaro("blanco", "traupis episcopus", "viudita", 10.2,"puntiagudo", 89)
 pajarito2=Pajaro("negro", "zanate", "idk", 19.2, "redondo",199)
 
 pajarito1.descripcion()
+print("------------------------")
 pajarito2.descripcion()
-pajarito1.crecimineto(10.6,90, "azulejo")
+pajarito1.crecimiento(10.6, 90, "azulejo")
+print("------POST CRECIMIENTO P1------")
 pajarito1.descripcion()
+
